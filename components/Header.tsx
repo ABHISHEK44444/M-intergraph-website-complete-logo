d07bae47faec68d6ffa53a2a31822c818bcbbdcd
ABHISHEK44444 <153176102+ABHISHEK44444@users.noreply.github.com>
2025-07-25 12:51:10 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -191,7 +191,8 @@ export const Header: React.FC<HeaderProps> = ({ onNavigate, currentPage, isScrol
     <>
       <header className={headerClasses}>
         <div className="container mx-auto px-2 sm:px-4">
-          <nav className="flex justify-between items-center py-4">
+          <nav className="flex justify-between items-center py-2 h-[64px] relative">
+
             <a href="#/home" onClick={(e) => handleNavClick(e, '#/home')} className="flex items-center space-x-2 sm:space-x-3">
                <MIntergraphLogo
                      className="text-white" // or any other tailwind classes
