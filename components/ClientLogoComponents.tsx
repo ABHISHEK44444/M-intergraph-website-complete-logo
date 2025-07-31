6f9ddb7e86e07ade1315e1e9989e252e546515cb
ABHISHEK44444 <153176102+ABHISHEK44444@users.noreply.github.com>
2025-07-31 16:31:20 +0530

    Update ClientLogoComponents.tsx

--- components/ClientLogoComponents.tsx
+++ components/ClientLogoComponents.tsx
@@ -196,7 +196,7 @@ export const NumericLogo: React.FC = () => (
            <img  
   src="images\numeric.png" 
   alt="Numeric logo"
-  class="h-18 w-22"
+  class="h-18 w-23"
 />
     </LogoWrapper>
 );
